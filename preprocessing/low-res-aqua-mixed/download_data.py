bcad082628c2b5afc94d76dd0dcb02f7731e2456
mserlin <marecserlin@gmail.com>
2024-08-02 18:22:58 -0700

    Commenting download_data.py

--- preprocessing/low-res-aqua-mixed/download_data.py
+++ preprocessing/low-res-aqua-mixed/download_data.py
@@ -11,25 +11,19 @@ import os
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
 
-
-api_token = {"username":"marecserlin","key":"25e86e23246017fb8b3fac0d7238dcfe"}
-import json
-
-with open('/root/.kaggle/kaggle.json', 'w') as file:
-    json.dump(api_token, file)
-
-
+#List of private datasets containing the entire the Low-Res dataset
 lowres_datasets = [
     "marecserlin/leap-001-002",
-    # 'ajobseeker/leap-003-part1',
-    # 'ajobseeker/leap-003-part2',
-    # 'ajobseeker/leap-004-part1',
-    # 'ajobseeker/leap-004-part2',
-    # 'ajobseeker/leap-005-part1',
-    # 'ajobseeker/leap-005-part2',
-    # 'marecserlin/leap-0006-0007-0008',
+    'ajobseeker/leap-003-part1',
+    'ajobseeker/leap-003-part2',
+    'ajobseeker/leap-004-part1',
+    'ajobseeker/leap-004-part2',
+    'ajobseeker/leap-005-part1',
+    'ajobseeker/leap-005-part2',
+    'marecserlin/leap-0006-0007-0008',
 ]
 
+#List of private datasets containing the entire the Aqua-Planet dataset
 aqua_datasets = [
     'yusefkaggle/leap-ocean'
 ]
@@ -38,10 +32,8 @@ if __name__ == '__main__':
     os.makedirs('./raw_lowres', exist_ok=True)
     commands = [f"kaggle datasets download {data} -p ./raw_lowres --unzip" for data in lowres_datasets]
 
-
-    # os.makedirs('./raw_aqua', exist_ok=True)
-    # commands = [f"kaggle datasets download {data} -p ./raw_aqua --unzip" for data in aqua_datasets]
-
+    os.makedirs('./raw_aqua', exist_ok=True)
+    commands = [f"kaggle datasets download {data} -p ./raw_aqua --unzip" for data in aqua_datasets]
 
     def run_command(command):
         subprocess.run(command, shell=True)
